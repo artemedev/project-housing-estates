b446de5417bbd688a0e0f569f23b2288e413213b
artemedev <artem.e.developer@gmail.com>
2021-07-22 11:24:23 +0300

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,42 @@
-$(function(){
+$(function () {
     $('.news__slider').slick({
+        prevArrow: '<button class="slick-btn slick-btn__left"><svg width="22" height="27" viewBox="0 0 22 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.72911e-07 13.5L21.75 0.942635L21.75 26.0574L1.72911e-07 13.5Z" fill="white"/></svg></button>',
+        nextArrow: '<button class="slick-btn slick-btn__right"><svg width="22" height="27" viewBox="0 0 22 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22 13.5L0.250001 26.0574L0.250002 0.942631L22 13.5Z" fill="white"/></svg></button>',
         arrows: true,
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+            {
+              breakpoint: 1023,
+              settings: {
+                slidesToShow: 1,
+                
+              }
+            }
+          ]
     });
-    $('.services__item-title').on('click',  function(){
+    $('.services__item-title').on('click', function () {
         $('.services__item').removeClass('services__item--active');
         $(this).parent().addClass('services__item--active');
     });
+    $('#menu').hide();
+
     $('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-        scrollHorizontally: true,
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: false,
         sectionSelector: '.page-section',
         scrollOverflow: true,
         verticalCentered: false,
         menu: '#header__nav',
         fixedElements: '#top',
-        anchors: ['top', 'about', 'porfolio', 'services', 'contacts', 'news'],
-        
+        anchors: ['top', 'about', 'porfolio', 'services', 'contacts', 'news']
     });
-  
-    $(".news__slider").slick({
-        nextArrow: `<button class="slider-btn slider-btn__right">
-        <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0.78161 17L9 9L0.78161 1"/>
-        </svg>
-     </button>
-     `,
-        prevArrow: `<button class="slider-btn slider-btn__left">
-        <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9.21839 1L1 9L9.21839 17"/>
-        </svg>
-     </button>
-     `,
-        infinite: false,
-     });    
 });
